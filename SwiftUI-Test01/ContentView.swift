5c6e1d1e6259bb0679e6905e48fcee49148b831e
moolnam <moolnam>
2022-06-15 16:29:05 +0900

    Initial Commit

---
+++ SwiftUI-Test01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Test01
+//
+//  Created by KimJongHee on 2022/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Test01/SwiftUI_Test01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Test01App.swift
+//  SwiftUI-Test01
+//
+//  Created by KimJongHee on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
